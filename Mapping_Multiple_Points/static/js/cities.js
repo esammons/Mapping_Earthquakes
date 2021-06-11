6cad72bcb818a7b44447277e70f1107133f29e6f
esammons <ebsammons@gmail.com>
2021-06-10 21:33:44 -0700

    message

--- Mapping_Multiple_Points/static/js/cities.js
+++ Mapping_Multiple_Points/static/js/cities.js
@@ -1,10 +1,3 @@
-// Add console.log to check to see if our code is working
-console.log("working")
-
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522,-118.2437],14);
-
-// We create the tile layer that will be the background of our map.
 // An array containing each city's location, state, and population.
 let cities = [{
     location: [40.7128, -74.0059],
@@ -37,17 +30,11 @@ let cities = [{
     population: 1660272
   }
   ];
-  // Get data from cities.js
-let cityData = cities;
   // Loop through the cities array and create one marker for each city.
-  cityData.forEach(function(city) {
+cities.forEach(function(city) {
     console.log(city)
-    L.cirleMarker(city.location, {
-        radius: city.population/10000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+    L.marker(city.location).addTo(map);
+   });
 //  Add a marker to the map for Los Angeles, California.
 //L.circle([34.0522, -118.2437], {
     //radius: 300,
